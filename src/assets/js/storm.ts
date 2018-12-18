9d2e1548e131aa51b869f367da78c0b22ca6108c
rhazen35 <rhazen35@gmail.com>
2018-12-18 23:53:39 +0100

    [Home images css changes]

--- src/assets/js/storm.ts
+++ src/assets/js/storm.ts
@@ -27,13 +27,13 @@ export class Storm {
             var h = canvas1.height = canvas2.height = canvas3.height = window.innerHeight;
 
             if (w < 2000) {
-                var rainthroughnum = 250;
-                var rainnum = 250;
+                rainthroughnum = 25;
+                rainnum = 25;
             }
 
             window.addEventListener('resize', function() {
-            w = canvas1.width = canvas2.width = canvas3.width = window.innerWidth;
-            h = canvas1.height = canvas2.height = canvas3.height = window.innerHeight;
+                w = canvas1.width = canvas2.width = canvas3.width = window.innerWidth;
+                h = canvas1.height = canvas2.height = canvas3.height = window.innerHeight;
             });
         }
     
@@ -43,34 +43,34 @@ export class Storm {
     
         function clearcanvas1() {
             if (ctx1) {
-            ctx1.clearRect(0, 0, w, h);
+                ctx1.clearRect(0, 0, w, h);
             }
         }
     
         function clearcanvas2() {
             if (ctx2) {
-            ctx2.clearRect(0, 0, canvas2.width, canvas2.height);
+                ctx2.clearRect(0, 0, canvas2.width, canvas2.height);
             }
         }
     
         function clearCanvas3() {
             if (ctx3) {
-            ctx3.globalCompositeOperation = 'destination-out';
-            ctx3.fillStyle = 'rgba(0,0,0,' + random(1, 30) / 100 + ')';
-            ctx3.fillRect(0, 0, w, h);
-            ctx3.globalCompositeOperation = 'source-over';
+                ctx3.globalCompositeOperation = 'destination-out';
+                ctx3.fillStyle = 'rgba(0,0,0,' + random(1, 30) / 100 + ')';
+                ctx3.fillRect(0, 0, w, h);
+                ctx3.globalCompositeOperation = 'source-over';
             }
         };
     
         function createRainTrough() {
             for (var i = 0; i < rainthroughnum; i++) {
                 RainTrough[i] = {
-                x: random(0, w),
-                y: random(0, h),
-                length: Math.floor(random(1, 830)),
-                opacity: Math.random() * 0.2,
-                xs: random(-2, 2),
-                ys: random(10, 20)
+                    x: random(0, w),
+                    y: random(0, h),
+                    length: Math.floor(random(1, 830)),
+                    opacity: Math.random() * 0.2,
+                    xs: random(-2, 2),
+                    ys: random(10, 20)
                 };
             }
         }
@@ -78,11 +78,11 @@ export class Storm {
         function createRain() {
             for (var i = 0; i < rainnum; i++) {
                 rain[i] = {
-                x: Math.random() * w,
-                y: Math.random() * h,
-                l: Math.random() * 1,
-                xs: -4 + Math.random() * 4 + 2,
-                ys: Math.random() * 10 + 10
+                    x: Math.random() * w,
+                    y: Math.random() * h,
+                    l: Math.random() * 1,
+                    xs: -4 + Math.random() * 4 + 2,
+                    ys: Math.random() * 10 + 10
                 };
             }
         }
@@ -94,15 +94,15 @@ export class Storm {
             var createCount = random(1, 3);
             for (var i = 0; i < createCount; i++) {
                 var single = {
-                x: x,
-                y: y,
-                xRange: random(5, 30),
-                yRange: random(10, 25),
-                path: [{
                     x: x,
-                    y: y
-                }],
-                pathLimit: random(40, 55)
+                    y: y,
+                    xRange: random(5, 30),
+                    yRange: random(10, 25),
+                    path: [{
+                        x: x,
+                        y: y
+                    }],
+                    pathLimit: random(40, 55)
                 };
                 lightning.push(single);
             }
@@ -111,27 +111,27 @@ export class Storm {
         function drawRainTrough(i) {
 
             if (ctx1) {
-            ctx1.beginPath();
-            var grd = ctx1.createLinearGradient(0, RainTrough[i].y, 0, RainTrough[i].y + RainTrough[i].length);
-            grd.addColorStop(0, "rgba(255,255,255,0)");
-            grd.addColorStop(1, "rgba(255,255,255," + RainTrough[i].opacity + ")");
-        
-            ctx1.fillStyle = grd;
-            ctx1.fillRect(RainTrough[i].x, RainTrough[i].y, 1, RainTrough[i].length);
-            ctx1.fill();
+                ctx1.beginPath();
+                var grd = ctx1.createLinearGradient(0, RainTrough[i].y, 0, RainTrough[i].y + RainTrough[i].length);
+                grd.addColorStop(0, "rgba(255,255,255,0)");
+                grd.addColorStop(1, "rgba(255,255,255," + RainTrough[i].opacity + ")");
+            
+                ctx1.fillStyle = grd;
+                ctx1.fillRect(RainTrough[i].x, RainTrough[i].y, 1, RainTrough[i].length);
+                ctx1.fill();
             }
         }
     
         function drawRain(i) {
 
             if (ctx2) {
-            ctx2.beginPath();
-            ctx2.moveTo(rain[i].x, rain[i].y);
-            ctx2.lineTo(rain[i].x + rain[i].l * rain[i].xs, rain[i].y + rain[i].l * rain[i].ys);
-            ctx2.strokeStyle = 'rgba(174,194,224,0.5)';
-            ctx2.lineWidth = 1;
-            ctx2.lineCap = 'round';
-            ctx2.stroke();
+                ctx2.beginPath();
+                ctx2.moveTo(rain[i].x, rain[i].y);
+                ctx2.lineTo(rain[i].x + rain[i].l * rain[i].xs, rain[i].y + rain[i].l * rain[i].ys);
+                ctx2.strokeStyle = 'rgba(174,194,224,0.5)';
+                ctx2.lineWidth = 1;
+                ctx2.lineCap = 'round';
+                ctx2.stroke();
             }
         }
     
@@ -142,8 +142,8 @@ export class Storm {
                 var light = lightning[i];
         
                 light.path.push({
-                x: light.path[light.path.length - 1].x + (random(0, light.xRange) - (light.xRange / 2)),
-                y: light.path[light.path.length - 1].y + (random(0, light.yRange))
+                    x: light.path[light.path.length - 1].x + (random(0, light.xRange) - (light.xRange / 2)),
+                    y: light.path[light.path.length - 1].y + (random(0, light.yRange))
                 });
         
                 if (light.path.length > light.pathLimit) {
@@ -164,7 +164,7 @@ export class Storm {
                 for (var pc = 0; pc < light.path.length; pc++) {
                     ctx3.lineTo(light.path[pc].x, light.path[pc].y);
                 }
-                if (Math.floor(random(0, 30)) === 1) { //to fos apo piso
+                if (Math.floor(random(0, 30)) === 1) {
                     ctx3.fillStyle = 'rgba(255, 255, 255, ' + random(1, 3) / 100 + ')';
                     ctx3.fillRect(0, 0, w, h);
                 }
@@ -178,9 +178,9 @@ export class Storm {
             clearcanvas1();
             for (var i = 0; i < rainthroughnum; i++) {
                 if (RainTrough[i].y >= h) {
-                RainTrough[i].y = h - RainTrough[i].y - RainTrough[i].length * 5;
+                    RainTrough[i].y = h - RainTrough[i].y - RainTrough[i].length * 5;
                 } else {
-                RainTrough[i].y += speedRainTrough;
+                    RainTrough[i].y += speedRainTrough;
                 }
                 drawRainTrough(i);
             }
@@ -192,8 +192,8 @@ export class Storm {
                 rain[i].x += rain[i].xs;
                 rain[i].y += rain[i].ys;
                 if (rain[i].x > w || rain[i].y > h) {
-                rain[i].x = Math.random() * w;
-                rain[i].y = -20;
+                    rain[i].x = Math.random() * w;
+                    rain[i].y = -20;
                 }
                 drawRain(i);
             }
@@ -205,7 +205,7 @@ export class Storm {
             if (lightTimeCurrent >= lightTimeTotal) {
                 createLightning();
                 lightTimeCurrent = 0;
-                lightTimeTotal = 200;  //rand(100, 200)
+                lightTimeTotal = 150;
             }
             drawLightning();
         }
